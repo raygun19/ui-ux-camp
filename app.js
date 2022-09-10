ea841e90f46c90fdeb167b68726d594c3b735255
9308233900 <reagan_hunt@brown.edu>
2022-09-10 17:56:54 -0400

    final

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,12 +40,20 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
 
+  
+  const result = fruitsArray.map(getName);
+
   // TODO: Create a new HTML element to display your data 
+  const newElement = document.createElement('div');
+  newElement.innerHTML = "new";
+
+  const existingElement = document.getElementById('box');
+  existingElement.append(result);
 
   // TODO: Append your new element to the page
 
@@ -58,6 +66,10 @@ const exampleAddElement = () => {
   newElement.innerHTML = "this text is inside a div";
 
   // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
+  const existingElement = document.getElementById('box');
   existingElement.append(newElement);
+}
+
+function getName(item) {
+  return item.name;
 }
